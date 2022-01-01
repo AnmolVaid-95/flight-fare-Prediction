ce4c7b4425ddf1b04341ed3e0e0b8279a8840c8d
AnmolVaid-95 <79400466+AnmolVaid-95@users.noreply.github.com>
2022-01-01 19:36:28 +0530

    Add files via upload

---
+++ Flight-Price-Prediction-master/app.py
@@ -0,0 +1,429 @@
+import os
+
+from flask import Flask, request, render_template
+from flask_cors import cross_origin
+import sklearn
+import pickle
+import pandas as pd
+import logging
+
+
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+# create file handler which logs to the specified file
+file_handler = logging.FileHandler('application_log_file.log')
+# Add the handler to the Logger
+logger.addHandler(file_handler)
+
+
+app = Flask(__name__)
+model = pickle.load(open("flight_rf.pkl", "rb"))
+
+
+
+@app.route("/")
+@cross_origin()
+def home():
+    return render_template("home.html")
+
+
+@app.route("/predict", methods = ["GET", "POST"])
+@cross_origin()
+def predict():
+    """
+            Method Name: predict
+            Description: predicts the fare based on input values from web portal.
+            Output: Will display price in INR, in float value
+            On Failure: Raise Exception
+
+            Written By: Anmol Vaid
+            Version: 1.0
+            Revisions: None
+
+    """
+    try :
+        if request.method == "POST":
+            logger.info("Below details are entered by User for this journey")
+            date_dep = request.form["Dep_Time"]
+            Journey_day = int(pd.to_datetime(date_dep, format="%Y-%m-%dT%H:%M").day)
+            logger.info("Day of journey is : {}".format(Journey_day))
+            Journey_month = int(pd.to_datetime(date_dep, format ="%Y-%m-%dT%H:%M").month)
+            logger.info("Month of journey is : {}".format(Journey_month))
+            # Above code will capture departure date
+
+            # Departure
+            Dep_hour = int(pd.to_datetime(date_dep, format ="%Y-%m-%dT%H:%M").hour)
+            logger.info("Departure hour of journey is : {}".format(Dep_hour))
+            Dep_min = int(pd.to_datetime(date_dep, format ="%Y-%m-%dT%H:%M").minute)
+            logger.info("Departure minutes of journey is : {}".format(Dep_min))
+            # print("Departure : ",Dep_hour, Dep_min)
+            # Above code will capture
+
+            # Arrival
+            date_arr = request.form["Arrival_Time"]
+            Arrival_hour = int(pd.to_datetime(date_arr, format ="%Y-%m-%dT%H:%M").hour)
+            logger.info("Arrival hour is : {}".format(Arrival_hour))
+            Arrival_min = int(pd.to_datetime(date_arr, format ="%Y-%m-%dT%H:%M").minute)
+            logger.info("Arrival minute is : {}".format(Arrival_min))
+            # print("Arrival : ", Arrival_hour, Arrival_min)
+
+            # Duration
+            dur_hour = abs(Arrival_hour - Dep_hour)
+            logger.info("Duration hour is : {}".format(dur_hour))
+            dur_min = abs(Arrival_min - Dep_min)
+            logger.info("Duration minutes is : {}".format(dur_min))
+            # print("Duration : ", dur_hour, dur_min)
+
+            # Total Stops
+            Total_stops = int(request.form["stops"])
+            logger.info("Total stop is : {} ".format(Total_stops))
+            # print(Total_stops)
+
+            # Airline
+            # AIR ASIA = 0 (not in column)
+            airline=request.form['airline']
+            if(airline=='Jet Airways'):
+                Jet_Airways = 1
+                IndiGo = 0
+                Air_India = 0
+                Multiple_carriers = 0
+                SpiceJet = 0
+                Vistara = 0
+                GoAir = 0
+                Multiple_carriers_Premium_economy = 0
+                Jet_Airways_Business = 0
+                Vistara_Premium_economy = 0
+                Trujet = 0
+                logger.info("Airline selected is : {}".format(airline))
+
+            elif (airline=='IndiGo'):
+                Jet_Airways = 0
+                IndiGo = 1
+                Air_India = 0
+                Multiple_carriers = 0
+                SpiceJet = 0
+                Vistara = 0
+                GoAir = 0
+                Multiple_carriers_Premium_economy = 0
+                Jet_Airways_Business = 0
+                Vistara_Premium_economy = 0
+                Trujet = 0
+                logger.info("Airline selected is : {}".format(airline))
+
+            elif (airline=='Air India'):
+                Jet_Airways = 0
+                IndiGo = 0
+                Air_India = 1
+                Multiple_carriers = 0
+                SpiceJet = 0
+                Vistara = 0
+                GoAir = 0
+                Multiple_carriers_Premium_economy = 0
+                Jet_Airways_Business = 0
+                Vistara_Premium_economy = 0
+                Trujet = 0
+                logger.info("Airline selected is : {}".format(airline))
+
+            elif (airline=='Multiple carriers'):
+                Jet_Airways = 0
+                IndiGo = 0
+                Air_India = 0
+                Multiple_carriers = 1
+                SpiceJet = 0
+                Vistara = 0
+                GoAir = 0
+                Multiple_carriers_Premium_economy = 0
+                Jet_Airways_Business = 0
+                Vistara_Premium_economy = 0
+                Trujet = 0
+                logger.info("Airline selected is : {}".format(airline))
+
+            elif (airline=='SpiceJet'):
+                Jet_Airways = 0
+                IndiGo = 0
+                Air_India = 0
+                Multiple_carriers = 0
+                SpiceJet = 1
+                Vistara = 0
+                GoAir = 0
+                Multiple_carriers_Premium_economy = 0
+                Jet_Airways_Business = 0
+                Vistara_Premium_economy = 0
+                Trujet = 0
+                logger.info("Airline selected is : {}".format(airline))
+
+            elif (airline=='Vistara'):
+                Jet_Airways = 0
+                IndiGo = 0
+                Air_India = 0
+                Multiple_carriers = 0
+                SpiceJet = 0
+                Vistara = 1
+                GoAir = 0
+                Multiple_carriers_Premium_economy = 0
+                Jet_Airways_Business = 0
+                Vistara_Premium_economy = 0
+                Trujet = 0
+                logger.info("Airline selected is : {}".format(airline))
+
+            elif (airline=='GoAir'):
+                Jet_Airways = 0
+                IndiGo = 0
+                Air_India = 0
+                Multiple_carriers = 0
+                SpiceJet = 0
+                Vistara = 0
+                GoAir = 1
+                Multiple_carriers_Premium_economy = 0
+                Jet_Airways_Business = 0
+                Vistara_Premium_economy = 0
+                Trujet = 0
+                logger.info("Airline selected is : {}".format(airline))
+
+            elif (airline=='Multiple carriers Premium economy'):
+                Jet_Airways = 0
+                IndiGo = 0
+                Air_India = 0
+                Multiple_carriers = 0
+                SpiceJet = 0
+                Vistara = 0
+                GoAir = 0
+                Multiple_carriers_Premium_economy = 1
+                Jet_Airways_Business = 0
+                Vistara_Premium_economy = 0
+                Trujet = 0
+                logger.info("Airline selected is : {}".format(airline))
+
+            elif (airline=='Jet Airways Business'):
+                Jet_Airways = 0
+                IndiGo = 0
+                Air_India = 0
+                Multiple_carriers = 0
+                SpiceJet = 0
+                Vistara = 0
+                GoAir = 0
+                Multiple_carriers_Premium_economy = 0
+                Jet_Airways_Business = 1
+                Vistara_Premium_economy = 0
+                Trujet = 0
+                logger.info("Airline selected is : {}".format(airline))
+
+            elif (airline=='Vistara Premium economy'):
+                Jet_Airways = 0
+                IndiGo = 0
+                Air_India = 0
+                Multiple_carriers = 0
+                SpiceJet = 0
+                Vistara = 0
+                GoAir = 0
+                Multiple_carriers_Premium_economy = 0
+                Jet_Airways_Business = 0
+                Vistara_Premium_economy = 1
+                Trujet = 0
+                logger.info("Airline selected is : {}".format(airline))
+
+            elif (airline=='Trujet'):
+                Jet_Airways = 0
+                IndiGo = 0
+                Air_India = 0
+                Multiple_carriers = 0
+                SpiceJet = 0
+                Vistara = 0
+                GoAir = 0
+                Multiple_carriers_Premium_economy = 0
+                Jet_Airways_Business = 0
+                Vistara_Premium_economy = 0
+                Trujet = 1
+                logger.info("Airline selected is : {}".format(airline))
+
+            else:
+                Jet_Airways = 0
+                IndiGo = 0
+                Air_India = 0
+                Multiple_carriers = 0
+                SpiceJet = 0
+                Vistara = 0
+                GoAir = 0
+                Multiple_carriers_Premium_economy = 0
+                Jet_Airways_Business = 0
+                Vistara_Premium_economy = 0
+                Trujet = 0
+                logger.info("Airline selected is : {}".format(airline))
+
+            # print(Jet_Airways,
+            #     IndiGo,
+            #     Air_India,
+            #     Multiple_carriers,
+            #     SpiceJet,
+            #     Vistara,
+            #     GoAir,
+            #     Multiple_carriers_Premium_economy,
+            #     Jet_Airways_Business,
+            #     Vistara_Premium_economy,
+            #     Trujet)
+
+            # Source
+            # Banglore = 0 (not in column)
+            Source = request.form["Source"]
+            if (Source == 'Delhi'):
+                s_Delhi = 1
+                s_Kolkata = 0
+                s_Mumbai = 0
+                s_Chennai = 0
+                logger.info("Source selected is : {}".format(Source))
+
+            elif (Source == 'Kolkata'):
+                s_Delhi = 0
+                s_Kolkata = 1
+                s_Mumbai = 0
+                s_Chennai = 0
+                logger.info("Source selected is : {}".format(Source))
+
+            elif (Source == 'Mumbai'):
+                s_Delhi = 0
+                s_Kolkata = 0
+                s_Mumbai = 1
+                s_Chennai = 0
+                logger.info("Source selected is : {}".format(Source))
+
+            elif (Source == 'Chennai'):
+                s_Delhi = 0
+                s_Kolkata = 0
+                s_Mumbai = 0
+                s_Chennai = 1
+                logger.info("Source selected is : {}".format(Source))
+
+            else:
+                s_Delhi = 0
+                s_Kolkata = 0
+                s_Mumbai = 0
+                s_Chennai = 0
+                logger.info("Source selected is : {}".format(Source))
+
+            # print(s_Delhi,
+            #     s_Kolkata,
+            #     s_Mumbai,
+            #     s_Chennai)
+
+            # Destination
+            # Banglore = 0 (not in column)
+            Source = request.form["Destination"]
+            if (Source == 'Cochin'):
+                d_Cochin = 1
+                d_Delhi = 0
+                d_New_Delhi = 0
+                d_Hyderabad = 0
+                d_Kolkata = 0
+                logger.info("Source selected is : {}".format(Source))
+
+            elif (Source == 'Delhi'):
+                d_Cochin = 0
+                d_Delhi = 1
+                d_New_Delhi = 0
+                d_Hyderabad = 0
+                d_Kolkata = 0
+                logger.info("Source selected is : {}".format(Source))
+
+            # elif (Source == 'New_Delhi'):
+            #     d_Cochin = 0
+            #     d_Delhi = 0
+            #     d_New_Delhi = 1
+            #     d_Hyderabad = 0
+            #     d_Kolkata = 0
+
+            elif (Source == 'Hyderabad'):
+                d_Cochin = 0
+                d_Delhi = 0
+                d_New_Delhi = 0
+                d_Hyderabad = 1
+                d_Kolkata = 0
+                logger.info("Source selected is : {}".format(Source))
+
+            elif (Source == 'Kolkata'):
+                d_Cochin = 0
+                d_Delhi = 0
+                d_New_Delhi = 0
+                d_Hyderabad = 0
+                d_Kolkata = 1
+                logger.info("Source selected is : {}".format(Source))
+
+            else:
+                d_Cochin = 0
+                d_Delhi = 0
+                d_New_Delhi = 0
+                d_Hyderabad = 0
+                d_Kolkata = 0
+                logger.info("Source selected is : {}".format(Source))
+
+            # print(
+            #     d_Cochin,
+            #     d_Delhi,
+            #     d_New_Delhi,
+            #     d_Hyderabad,
+            #     d_Kolkata
+            # )
+
+
+        #     ['Total_Stops', 'Journey_day', 'Journey_month', 'Dep_hour',
+        #    'Dep_min', 'Arrival_hour', 'Arrival_min', 'Duration_hours',
+        #    'Duration_mins', 'Airline_Air India', 'Airline_GoAir', 'Airline_IndiGo',
+        #    'Airline_Jet Airways', 'Airline_Jet Airways Business',
+        #    'Airline_Multiple carriers',
+        #    'Airline_Multiple carriers Premium economy', 'Airline_SpiceJet',
+        #    'Airline_Trujet', 'Airline_Vistara', 'Airline_Vistara Premium economy',
+        #    'Source_Chennai', 'Source_Delhi', 'Source_Kolkata', 'Source_Mumbai',
+        #    'Destination_Cochin', 'Destination_Delhi', 'Destination_Hyderabad',
+        #    'Destination_Kolkata', 'Destination_New Delhi']
+
+            prediction=model.predict([[
+                Total_stops,
+                Journey_day,
+                Journey_month,
+                Dep_hour,
+                Dep_min,
+                Arrival_hour,
+                Arrival_min,
+                dur_hour,
+                dur_min,
+                Air_India,
+                GoAir,
+                IndiGo,
+                Jet_Airways,
+                Jet_Airways_Business,
+                Multiple_carriers,
+                Multiple_carriers_Premium_economy,
+                SpiceJet,
+                Trujet,
+                Vistara,
+                Vistara_Premium_economy,
+                s_Chennai,
+                s_Delhi,
+                s_Kolkata,
+                s_Mumbai,
+                d_Cochin,
+                d_Delhi,
+                d_Hyderabad,
+                d_Kolkata,
+                d_New_Delhi
+            ]])
+
+            output=round(prediction[0],2)
+
+            logger.info("Predicted fair is Rs. : {}".format(output))
+
+            return render_template('home.html',prediction_text="Your Flight price is Rs. {}".format(output))
+
+        return render_template("home.html")
+
+    except Exception as e:
+        print("Please check the logging file for error.")
+        logger.error("Your error is : {}".format(e))
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
